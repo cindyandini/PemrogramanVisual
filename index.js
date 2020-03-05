09a94923b7c41ff55517e20faa72143eb26c4db1
cindyandini <57897008+cindyandini@users.noreply.github.com>
2020-03-05 16:00:47 +0800

    challenge part 1

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const {app,BrowserWindow, Menu, ipcMain} = electron;
 let todayWindow;
 let createWindow;
 let listWindow;
+let aboutWindow;
 
 app.on("ready", ()=>{
     todayWindow = new BrowserWindow({
@@ -51,6 +52,21 @@ const createWindowCreator = () =>{
     createWindow.loadURL(`file://${__dirname}/create.html`);
     createWindow.on("closed", ()=>(createWindow = null));
 };
+
+const aboutWindowCreator = () =>{
+    aboutWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width : 600,
+        height : 400,
+        title:"All Appointments"
+    });
+    aboutWindow.setMenu(null);
+    aboutWindow.loadURL(`file://${__dirname}/about.html`);
+    aboutWindow.on("closed", ()=>(aboutWindow = null));
+};
+
 ipcMain.on("appointment:create",(event,appointment)=>{
     console.log(appointment);
 });
@@ -80,6 +96,12 @@ const menuTemplate = [{
     {
         label: "View",
         submenu: [{role: "reload"}, {role: "toggledevtools"}]
+    },
+    {
+        label: "About",
+        click(){
+            aboutWindowCreator();
+        }
     }
 
 ]
